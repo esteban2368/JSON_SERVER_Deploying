50e01dfc527dc9915415785d5a8e9ef6043b1827
esteban2368 <esteban.d2168@gmail.com>
2023-12-21 22:49:35 -0500

    install json server and create rest Api

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("./db/db.json");
+const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000);
 
